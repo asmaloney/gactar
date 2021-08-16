1c950fffa63aa37a2be19caa0f764200e2caa06d
Andy Maloney <asmaloney@gmail.com>
2021-08-15 20:46:09 -0400

    Change name of virtual env dir from "pyenv" to "env" since we'll be putting everything there

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,10 +8,10 @@ fi
 cd .. || exit
 
 # create the virtual env dir
-python3 -m venv pyenv
+python3 -m venv env
 
 # activate it
-source ./pyenv/bin/activate
+source ./env/bin/activate
 
 echo "Using python3 from here:" `which python3`
 python3 --version
@@ -19,14 +19,14 @@ python3 --version
 # update pip
 pip install --upgrade pip
 
-# install pyactor - should we use requirements.txt?
-pip install pyactr
+# should we use requirements.txt?
+pip install wget pyactr
 
 # Because CCMSuite isn't a proper package, we need to copy files to the right place.
 # I'm using a fork (CCM-PyACTR) to avoid pulling all the tmp and .pyc files in the original repo.
-cd ./pyenv
+cd ./env
 git clone https://github.com/asmaloney/CCM-PyACTR.git
 cp -rpf CCM-PyACTR/ccm lib/python3.9/site-packages/
 
 echo "Your environment is set up."
-echo "To load your virtual enviroment, run: source ./pyenv/bin/activate"
+echo "To load your virtual enviroment, run: source ./env/bin/activate"
