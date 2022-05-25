dc5e3171f61bdc9c5427f4229251df3065676e92
Andy Maloney <asmaloney@gmail.com>
2022-05-25 13:54:04 -0400

    Rework amodlog to prepare for better error handling
    - make "level" a string so it's easier to return in JSON
    - change "entry" struct to "Issue"
    - add JSON tags to Issue for web server returns
    - add a way to get all issues at once (for web server returns)

--- amodlog/amodlog.go
+++ amodlog/amodlog.go
@@ -7,32 +7,25 @@ import (
 	"strings"
 )
 
-type level int
+type level string
 
 const (
-	info level = iota
-	err
+	info level = "info"
+	err  level = "error"
 )
 
-type entry struct {
-	level level
-	line  int
-	text  string
+type Issue struct {
+	Level level  `json:"level"`
+	Line  int    `json:"lineNumber,omitempty"`
+	Text  string `json:"text"`
 }
+
+type IssueList = []Issue
 
 type Log struct {
 	hasInfo  bool // does this log contain at least one info entry?
 	hasError bool // does this log contain at least one error entry?
-	entries  []entry
-}
-
-func (el *Log) addEntry(line int, l level, e string, a ...interface{}) {
-	str := fmt.Sprintf(e, a...)
-	el.entries = append(el.entries, entry{
-		level: l,
-		line:  line,
-		text:  str,
-	})
+	issues   []Issue
 }
 
 // New will create and return a new Log.
@@ -40,17 +33,22 @@ func New() *Log {
 	return &Log{
 		hasInfo:  false,
 		hasError: false,
-		entries:  []entry{},
+		issues:   []Issue{},
 	}
 }
 
+// AllIssues returns a slice of all the current issues in the log.
+func (l Log) AllIssues() IssueList {
+	return l.issues
+}
+
 // HasInfo returns whether this log contains at least one info entry.
-func (l *Log) HasInfo() bool {
+func (l Log) HasInfo() bool {
 	return l.hasInfo
 }
 
 // HasError returns whether this log contains at least one error entry.
-func (l *Log) HasError() bool {
+func (l Log) HasError() bool {
 	return l.hasError
 }
 
@@ -77,35 +75,44 @@ func (l *Log) String() string {
 // This is used when parsing goals input by the user.
 // For the UX we want to manage the output text differently.
 // See amod.go ParseChunk()
-func (l *Log) FirstEntry() string {
-	if len(l.entries) == 0 {
+func (l Log) FirstEntry() string {
+	if len(l.issues) == 0 {
 		return "<INTERNAL ERROR: no Log entries exist>"
 	}
 
-	return l.entries[0].text
+	return l.issues[0].Text
 }
 
 // Write will write the entire log. It will prepend INFO/ERROR and append
 // line numbers (if any) to each log entry.
-func (l *Log) Write(w io.Writer) {
-	for _, entry := range l.entries {
+func (l Log) Write(w io.Writer) {
+	for _, entry := range l.issues {
 		var str string
 
-		switch entry.level {
+		switch entry.Level {
 		case info:
 			str = "INFO: "
 		case err:
 			str = "ERROR: "
 		}
 
-		str += entry.text
+		str += entry.Text
 
-		if entry.line != 0 {
-			str += fmt.Sprintf(" (line %d)", entry.line)
+		if entry.Line != 0 {
+			str += fmt.Sprintf(" (line %d)", entry.Line)
 		}
 
 		str += "\n"
 
 		w.Write([]byte(str))
 	}
+}
+
+func (el *Log) addEntry(line int, l level, e string, a ...interface{}) {
+	str := fmt.Sprintf(e, a...)
+	el.issues = append(el.issues, Issue{
+		Level: l,
+		Line:  line,
+		Text:  str,
+	})
 }
