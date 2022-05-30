7c34520264355e76bd9714ec15ee7fd474a7a8a2
Andy Maloney <asmaloney@gmail.com>
2022-05-30 10:18:36 -0400

    Output the initial goal as an "info" issue
    Fixes asmaloney/gactar-vscode#3

--- util/validate/validate.go
+++ util/validate/validate.go
@@ -6,9 +6,18 @@ import (
 	"github.com/asmaloney/gactar/issues"
 )
 
-// Goal adds a warning if we don't have a goal.
+// Goal adds a warning if we don't have a goal or adds info with the initial goal.
 func Goal(model *actr.Model, initialGoal string, log *issues.Log) {
-	if initialGoal == "" && !model.HasInitializer("buffer") {
+	initializer := model.LookupInitializer("goal")
+	if initialGoal == "" && initializer == nil {
 		log.Warning(nil, "initial goal not provided and it was not initialized in the init section")
+
+		return
 	}
+
+	if initialGoal == "" {
+		initialGoal = initializer.Pattern.String()
+	}
+
+	log.Info(nil, "initial goal is %s", initialGoal)
 }
