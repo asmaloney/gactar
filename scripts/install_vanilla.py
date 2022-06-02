371381044a952c64efb528d697fecce8b17fe01c
Andy Maloney <asmaloney@gmail.com>
2022-06-02 11:32:50 -0400

    {setup} Rename and update script to install vanilla ACT-R & sbcl compiler

--- scripts/install_vanilla.py
+++ scripts/install_vanilla.py
@@ -1,4 +1,4 @@
-"""The get_implementations module will download & install multiple ACT-R frameworks."""
+"""The install_vanilla module will download & install ACT-R (Lisp) and the SBCL Lisp compiler if possible."""
 import os
 import platform
 import shutil
@@ -17,7 +17,7 @@ import requests
 #   https://github.com/asmaloney/gactar/pulls
 
 
-def remove_file(file_name):
+def remove_file(file_name: str):
     """Remove a file from the file system."""
     if os.path.isfile(file_name):
         try:
@@ -27,7 +27,7 @@ def remove_file(file_name):
             sys.exit()
 
 
-def remove_dir(dir_name):
+def remove_dir(dir_name: str):
     """Remove a directory from the file system."""
     if os.path.isdir(dir_name):
         try:
@@ -37,13 +37,13 @@ def remove_dir(dir_name):
             sys.exit()
 
 
-def unpack_file(file_name):
+def unpack_file(file_name: str):
     """Unpack a file."""
     if os.path.isfile(file_name):
         shutil.unpack_archive(file_name)
 
 
-def download_url(url):
+def download_url(url: str) -> str:
     """Download a URL."""
     local_filename = url.split('/')[-1]
     with requests.get(url, stream=True) as response:
@@ -57,11 +57,10 @@ def download_vanilla():
     """Download the lisp ACT-R files and install in the correct location."""
     print('Downloading and installing Vanilla ACT-R...')
 
-    version = "v7.27.0"
+    version = 'v7.27.0'
 
-    url = 'https://github.com/asmaloney/ACT-R/releases/download/' + \
-        version + '/actr-super-slim-' + version + '.zip'
-    unpacked_dir = 'actr-super-slim-' + version
+    url = f'https://github.com/asmaloney/ACT-R/releases/download/{version}/actr-super-slim-{version}.zip'
+    unpacked_dir = f'actr-super-slim-{version}'
     target_dir = 'actr'
 
     # remove old files if they exists for some reason
@@ -81,7 +80,7 @@ def download_vanilla():
     os.chdir('..')
 
 
-def download_sbcl():
+def download_sbcl() -> str:
     """Download the SBCL (Lisp) compiler and install in the correct location."""
     # See: http://www.sbcl.org/platform-table.html
     system = platform.system()
@@ -91,48 +90,57 @@ def download_sbcl():
         print('Downloading and installing SBCL...')
         system = 'darwin'
 
-        version = "2.1.2"  # latest arm64 version
+        version = '2.1.2'  # latest arm64 version
 
         if arch == 'x86_64':
-            version = "1.2.11"  # latest x86_64 version
+            version = '1.2.11'  # latest x86_64 version
             arch = 'x86-64'     # URL needs hyphen
 
-        base_dir = pathlib.Path().resolve()
-
-        dir_name = 'sbcl-' + version + '-' + arch + '-' + system
-        unpacked_dir = dir_name
-        compressed_file = dir_name + '-binary.tar.bz2'
-        url = 'https://prdownloads.sourceforge.net/sbcl/' + compressed_file
-
-        # remove old file if it exists for some reason
-        remove_dir(unpacked_dir)
-
-        # download sbcl
-        compressed_file = download_url(url)
-        unpack_file(compressed_file)
-
-        # run the install
-        os.environ['INSTALL_ROOT'] = str(base_dir)
-        os.chdir(dir_name)
-        subprocess.run(['./install.sh'], check=True, env=os.environ)
-        os.chdir(base_dir)
-
-        # compile the actr files
-        os.unsetenv('INSTALL_ROOT')
-        os.environ['SBCL_HOME'] = str(base_dir / 'lib/sbcl')
-        subprocess.run(['./bin/sbcl', '--script',
-                        'actr/load-single-threaded-act-r.lisp'], check=True, env=os.environ)
-
-        # clean up
-        remove_file(compressed_file)
-        remove_dir(unpacked_dir)
+        dir_name = f'sbcl-{version}-{arch}-{system}'
     else:
-        print(
-            "ERROR: I don't know how to install the sbcl compiler for your platform ("
-            + system + ' - ' + arch + ')')
-        print("Please see the gactar README for how to download and setup sbcl.")
+        raise Exception(
+            f'ERROR: I don\'t know how to install the sbcl compiler for your platform({system} - {arch})\n'
+            'Please see the gactar README for how to download and setup sbcl.'
+        )
+
+    # remove old file if it exists for some reason
+    remove_dir(dir_name)
+
+    # download sbcl
+    url = f'https://prdownloads.sourceforge.net/sbcl/{dir_name}-binary.tar.bz2'
+
+    compressed_file = download_url(url)
+    unpack_file(compressed_file)
+
+    return dir_name
+
+
+def install_vanilla(dir_name: str):
+    print(f'Installing from {dir_name}')
+
+    base_dir = pathlib.Path().resolve()
+
+    # run the install
+    os.environ['INSTALL_ROOT'] = str(base_dir)
+    os.chdir(dir_name)
+    subprocess.run(['./install.sh'], check=True, env=os.environ)
+    os.chdir(base_dir)
+
+    # compile the actr files
+    os.unsetenv('INSTALL_ROOT')
+    os.environ['SBCL_HOME'] = str(base_dir / 'lib/sbcl')
+    subprocess.run(['./bin/sbcl', '--script',
+                    'actr/load-single-threaded-act-r.lisp'], check=True, env=os.environ)
+
+    # clean up
+    remove_dir(dir_name)
 
 
 if __name__ == "__main__":
-    download_vanilla()
-    download_sbcl()
+    try:
+        # download_vanilla()
+        dir_name = download_sbcl()
+        install_vanilla(dir_name)
+    except BaseException as err:
+        print(err)
+        sys.exit()
