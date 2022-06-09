8213ad754ba0a026df8d1c8a3473ab12bb17c059
Andy Maloney <asmaloney@gmail.com>
2022-06-09 13:31:47 -0400

    {lex} Fix fuzzing findings for numbers
    Properly lex "+.", "+.9", "0.", ".42.5"

--- amod/lex_test.go
+++ amod/lex_test.go
@@ -11,13 +11,13 @@ func TestNumber(t *testing.T) {
 	t.Parallel()
 
 	// https://github.com/asmaloney/gactar/issues/2
-	src := `0 0.3 5 55.6 .9`
+	src := `0 0.3 5 55.6 .9 +6 -0.7 +.9 0.`
 
 	l := lex("test", src)
 
-	expecteds := strings.Split(src, " ")
+	expectedResults := strings.Split(src, " ")
 
-	for i, expected := range expecteds {
+	for i, expected := range expectedResults {
 		token, err := l.Next()
 		if err != nil {
 			t.Errorf("[index %d] error getting next token: %s", i, err.Error())
@@ -72,5 +72,38 @@ func TestForwardSlashNotComment(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("unexpected error: %q", err.Error())
+	}
+}
+
+func TestNumberDoubleDecimal(t *testing.T) {
+	t.Parallel()
+
+	l := lex("test", ".42.5")
+
+	token, err := l.Next()
+
+	if err != nil {
+		t.Errorf("unexpected error: %q", err.Error())
+	}
+
+	expected := ".42"
+	if token.Value != expected {
+		t.Errorf("did not lex number properly: got %q (%#v), expected %q", token.Value, token, expected)
+	}
+}
+
+func TestNumberNoDigits(t *testing.T) {
+	t.Parallel()
+
+	l := lex("test", "+.")
+
+	token, err := l.Next()
+
+	if err != nil {
+		t.Errorf("unexpected error: %q", err.Error())
+	}
+
+	if token.Type != lexer.TokenType(lexemeChar) {
+		t.Errorf("expected to lex %q as char (%d) - got type %d", token.Value, lexemeChar, token.Type)
 	}
 }
