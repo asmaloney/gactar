aa2626ab741bae6625872624c8bcfbeed504addf
Andy Maloney <asmaloney@gmail.com>
2022-06-21 12:46:17 -0400

    Add container.FindAndDelete to remove items from slices (#239)

--- util/container/container_test.go
+++ util/container/container_test.go
@@ -70,3 +70,22 @@ func TestUniqueAndSorted(t *testing.T) {
 		}
 	}
 }
+
+func TestFindAndDelete(t *testing.T) {
+	t.Parallel()
+
+	list := []string{"a", "b", "c", "b", "d", "c"}
+
+	list2 := FindAndDelete(list, "b")
+
+	if len(list2) != 4 {
+		t.Errorf("Items not removed from list")
+	}
+
+	expected := []string{"a", "c", "d", "c"}
+	for i, v := range expected {
+		if v != list2[i] {
+			t.Errorf("Resulting list incorrect")
+		}
+	}
+}
