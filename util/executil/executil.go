875c7bebd1cf024dac4e3737d614c133bf9d74ae
Andy Maloney <asmaloney@gmail.com>
2023-08-09 16:32:26 -0400

    When debug is on, output the commands used to run frameworks (#370)

--- util/executil/executil.go
+++ util/executil/executil.go
@@ -6,6 +6,10 @@ import (
 	"os/exec"
 )
 
+var (
+	debugging bool = false
+)
+
 type ErrExecuteCommand struct {
 	Output string
 }
@@ -16,12 +20,31 @@ func (e ErrExecuteCommand) Error() string {
 
 func ExecCommand(name string, arg ...string) (output string, err error) {
 	cmd := exec.Command(name, arg...)
+
+	if debugging {
+		fmt.Printf("Executing: %s\n", cmd.String())
+	}
+
 	outputBytes, err := cmd.CombinedOutput()
 	output = string(outputBytes)
+
+	if debugging {
+		if err != nil {
+			fmt.Printf("Exec FAIL: %s\n%s\n", cmd.String(), output)
+		} else {
+			fmt.Printf("Exec SUCCESS: %s\n", cmd.String())
+		}
+	}
+
 	if err != nil {
 		err = &ErrExecuteCommand{Output: output}
 		return
 	}
 
 	return
+}
+
+// SetDebug turns debugging on and off. This will output the command before executing it.
+func SetDebug(debug bool) {
+	debugging = debug
 }
